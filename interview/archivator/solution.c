53aadf6cf68b38917dfd6d8dfaf5f67295252099
Mikhail <mz14102005@gmail.com>
2024-06-28 23:11:47 +0300

    the second task

--- interview/archivator/solution.c
+++ interview/archivator/solution.c
@@ -26,6 +26,35 @@
 char *archiving(char *input)
 {
     // Your code here
-    return input;
+    int n = strlen(input);
+    if (n==0){
+        return strdup("");
+    }
+    
+    char *result = (char *)malloc(2 * n + 1);
+    
+    int pos = 0;//Позиция для записи в результирующую строку
+    int i = 0;
+        while (i < n) {
+            char current_char = input[i];
+            int count = 1; //счетчик одинаковых символов
+            
+            while ((i + 1) < n && input[i + 1] == current_char) {
+                count++;
+                ++i;
+            }
+
+            if (count > 1) {
+                pos += sprintf(result + pos, "%d%c", count, current_char);
+            } else {
+                result[pos++] = current_char;
+            }
+
+            ++i;
+        }
+    
+    result[pos] = '\0'; //Завершаем строку
+    
+    return result;
 }
 
