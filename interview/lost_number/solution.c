1e1f6be2c2f34ce4397d78ca6b7e6527bdb201ca
Mikhail <mz14102005@gmail.com>
2024-06-29 01:16:42 +0300

    the fourth task

--- interview/lost_number/solution.c
+++ interview/lost_number/solution.c
@@ -14,7 +14,18 @@
 
 int lost_number(int* a, size_t size)
 {
-    // Your code here
-    return 0;
+    if(size == 0){
+        return 0;
+    }
+    
+    size_t N = size +1;
+    int exp_sum = (N*(N+1))/2; //ожидаемая сумма
+    
+    int real_sum = 0; //наша сумма
+    for(int i = 0; i<size; ++i){
+        real_sum += a[i];
+    }
+    
+    return exp_sum - real_sum;
 }
 
